f2cf83a81d07a7e3de753adc7e14720f8ebf27fc
mobyan <gomypig@gmail.com>
2017-01-10 12:59:03 +0800

    init laravel project

---
+++ doc/models.sql
@@ -0,0 +1,80 @@
+CREATE TABLE `user` (
+  -- 主键ID，必须
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  -- 名称与描述，可选
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '名称',
+  `description` varchar(2000) NOT NULL DEFAULT '' COMMENT '描述',
+  -- updated_at：修改时间, created_at：创建时间，必须
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更改时间',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+  -- deleted_at: 删除时间，需要实现软删除时使用，可选
+  `deleted_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
+  -- 权重，排序使用，可选
+  `wt` bigint(20) NOT NULL DEFAULT '0' COMMENT '权重',
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='信息';
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '名称',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更改时间',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `deleted_at` timestamp NOT NULL DEFAULT 0 COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='信息';
+
+CREATE TABLE `station` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '名称',
+  `lat` DECIMAL(10, 8) NOT NULL COMMENT '纬度',
+  `lng` DECIMAL(11, 8) NOT NULL COMMENT '经度',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更改时间',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `deleted_at` timestamp NOT NULL DEFAULT 0 COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='信息';
+
+CREATE TABLE `device` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `user_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
+  `station_id` bigint(20) unsigned NOT NULL COMMENT '基站ID',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '名称',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更改时间',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='设备信息';
+
+CREATE TABLE `device_config` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '名称',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更改时间',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='设备信息';
+
+CREATE TABLE `device_data` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `device_id` bigint(20) unsigned NOT NULL COMMENT '设备ID',
+  `device_config_id` bigint(20) unsigned NOT NULL COMMENT '设备配置ID',
+  `ts` timestamp NOT NULL COMMENT '数据采集时间',
+  `data` json NOT NULL COMMENT '数据',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更改时间',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='设备信息';
+
+REATE TABLE `station_alert_config` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `station_id` bigint(20) unsigned NOT NULL COMMENT '设备ID',
+  `device_id` bigint(20) unsigned NOT NULL COMMENT '设备ID',
+  `device_data_key` varchar(20) unsigned NOT NULL COMMENT '设备ID',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更改时间',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='设备信息';
+
+
