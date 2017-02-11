d8c51cf078204bc2b517883ec4273a5e10e37896
mobyan <gomypig@gmail.com>
2017-02-11 16:39:22 +0800

    stations view to vue

---
+++ src/resources/assets/js/components/stations.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <div id="head" style="border-bottom: 1px solid #cacfda;margin-bottom: 1em;">
+      <div id="header" style="width: 90%;margin: auto;">
+        <div id="title">
+          <h3>Station 列表</h3>
+          <!-- <h3>xxxxxx</h3> -->
+        </div>
+      </div>
+    </div>
+    <div class="" style="width: 90%;margin: auto;">
+      <table class="table table-bordered table-striped table-hover">
+        <tr class="fatal">
+          <th>名称</th>
+          <th>类型</th>
+          <th>地址</th>
+          <th>状态</th>
+          <th>操作</th>
+        </tr>
+        <template v-for="station in stations">
+          <tr class="">
+            <!-- <td>{{ station.id }}</td> -->
+            <td>{{ station.name }}</td>
+            <td>{{ station.type }}</td>
+            <td>{{ station.location }}</td>
+            <td><img height="20" src="https://et.cern.ac.cn/static/images/3.png" class="signal"></td>
+            <td>
+              <a :href="'/station/' + station.id">
+                <img height="20" src="https://et.cern.ac.cn/static/images/info.png" class="signal">
+              </a>
+              <router-link to="/dashboard"><img height="20" src="https://et.cern.ac.cn/static/images/dashboard.png" class="signal"></router-link>
+              </a>
+            </td>
+          </tr>
+        </template>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        stations: []
+      }
+    },
+    mounted: function () {
+      var self = this;
+      $.get('/api/station', function (stations) {
+        self.stations = stations.items;
+      })
+    }
+  }
+</script>
