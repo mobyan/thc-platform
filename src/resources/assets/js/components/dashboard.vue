fe2dda7db16586a51a08fae9ee3e8de55c7f3048
mobyan <gomypig@gmail.com>
2017-02-19 21:30:02 +0800

    add station images

--- src/resources/assets/js/components/dashboard.vue
+++ src/resources/assets/js/components/dashboard.vue
@@ -1,77 +1,110 @@
 <template>
   <div class="">
-      <label>设备：</label><select class="form-control" v-model="selectedDevice">
-        <option v-for="(device,k) in devices" :value="k" >{{ device.name }}</option>
-    </select>
-    <highcharts v-for="chart in charts" :options="chart" ref="highcharts"></highcharts>
+    <label>设备：</label><select class="form-control" v-model="selectedDevice">
+    <option v-for="(device,k) in devices" :value="k" >{{ device.name }}</option>
+  </select>
+  <highcharts v-for="chart in charts" :options="chart" ref="highcharts"></highcharts>
+  <div>
+    <div>
+      <h3>基站图片</h3>
+    </div>
+    <div class="row">
+      <div v-for="image in images" class="col-xs-6 col-md-3">
+        <div class="thumbnail">
+          <img :src="image.url" alt="image.alt">
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-var defaultOptions = require('../chartOptions');
+  var defaultOptions = require('../chartOptions');
 
-export default  {
-  name: 'my-dashboard',
-  data : function () {
-    return {
-      devices: [],
-      station: null,
-      selectedDevice: 0,
-      datas: [],
-    }
-  },
-  computed: {
-    charts: function() {
-      var charts = {};
-      this.datas.forEach(function (v) {
+  export default  {
+    name: 'my-dashboard',
+    data : function () {
+      return {
+        devices: [],
+        station: null,
+        selectedDevice: 0,
+        datas: [],
+        images: [
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+        {url:'/image/1.jpg'},
+
+        ]
+      }
+    },
+    computed: {
+      charts: function() {
+        var charts = {};
+        this.datas.forEach(function (v) {
         // var type = ['temp', 'speed'][Math.floor(Math.random() * 10) % 2]; // fortest
         var type = v._type;
         charts[type] = charts[type] || _.cloneDeep(defaultOptions[type]);
         charts[type].series.push(v);
       })
-      return charts;
+        return charts;
+      },
     },
-  },
-  watch: {
-    selectedDevice: function () {
-      this.loadDeviceData(this.devices[this.selectedDevice])
-    }
-  },
-  methods: {
-    loadDeviceData: function (device) {
-      var app = this;
-      $.get('/api/station/'+this.station.id+'/device/'+device.id+'/data', function (data) {
-        app.datas = formatData(data.items, device.config);
+    watch: {
+      selectedDevice: function () {
+        this.loadDeviceData(this.devices[this.selectedDevice])
+      }
+    },
+    methods: {
+      loadDeviceData: function (device) {
+        var app = this;
+        $.get('/api/station/'+this.station.id+'/device/'+device.id+'/data', function (data) {
+          app.datas = formatData(data.items, device.config);
+        });
+      }
+    },
+    created:function () {
+      var self = this;
+      $.get('/api/station/'+ this.$route.params.station+'?with=devices.configs', function (station) {
+        station.devices = _.map(station.devices, function (v) {
+          v.config = _.last(v.configs);
+          return v;
+        })
+        self.devices = station.devices;
+        self.station = station;
       });
     }
-  },
-  created:function () {
-    var self = this;
-    $.get('/api/station/'+ this.$route.params.station+'?with=devices.configs', function (station) {
-      station.devices = _.map(station.devices, function (v) {
-        v.config = _.last(v.configs);
-        return v;
+  };
+  function formatData(items, config) {
+    var data = {};
+    items.forEach(function (v) {
+      _.forIn(v.data, function (value, key) {
+        data[key] = data[key] || [];
+        data[key].push([new Date(v.ts).getTime(), value.value]);
       })
-      self.devices = station.devices;
-      self.station = station;
-    });
-  }
-};
-function formatData(items, config) {
-  var data = {};
-  items.forEach(function (v) {
-    _.forIn(v.data, function (value, key) {
-      data[key] = data[key] || [];
-      data[key].push([new Date(v.ts).getTime(), value.value]);
     })
-  })
-  return _.map(data, function (v, k) {
-    return {
-      name: k,
-      data: v,
-      _type: config.data[k] ? config.data[k].type : 'temp',
-    }
-  })
-}
+    return _.map(data, function (v, k) {
+      return {
+        name: k,
+        data: v,
+        _type: config.data[k] ? config.data[k].type : 'temp',
+      }
+    })
+  }
 
 </script>
