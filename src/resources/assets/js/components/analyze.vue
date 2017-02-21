e70fa42d1de0b4e5c37d51c5a30200c3419bba17
mobyan <gomypig@gmail.com>
2017-02-21 13:21:39 +0000

    add datepicker

---
+++ src/resources/assets/js/components/analyze.vue
@@ -0,0 +1,73 @@
+<script>
+    import myDatepicker from 'vue-datepicker'
+    export default {
+      data () {
+        return {
+          startTime: {
+            time: '2017-02-11'
+        },
+        endtime: {
+            time: '2017-11-01'
+        },
+        option: {
+            type: 'day',
+            week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+            month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            format: 'YYYY-MM-DD',
+            placeholder: 'when?',
+            inputStyle: {
+              'display': 'inline-block',
+              'padding': '6px',
+              'line-height': '22px',
+              'font-size': '16px',
+              'border': '2px solid #fff',
+              'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.2)',
+              'border-radius': '2px',
+              'color': '#5F5F5F'
+          },
+          color: {
+              header: '#ccc',
+              headerText: '#f00'
+          },
+          buttons: {
+              ok: 'Ok',
+              cancel: 'Cancel'
+          },
+        overlayOpacity: 0.5, // 0.5 as default
+        dismissible: true // as true as default
+    },
+    timeoption: {
+        type: 'min',
+        week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+        month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        format: 'YYYY-MM-DD HH:mm'
+    },
+    multiOption: {
+        type: 'multi-day',
+        week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+        month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        format:"YYYY-MM-DD HH:mm"
+    },
+    limit: [
+    {
+        type: 'fromto',
+        from: '2017-02-01',
+        to: '2017-12-20'
+    }]
+}
+},
+components: {
+    'date-picker': myDatepicker
+}
+}
+</script>
+<template>
+  <div class="card">
+    <!-- for Vue 2.0 -->
+    <div class="row">
+      <span>Departure Date：</span>
+      <date-picker :date="startTime" :option="option" :limit="limit"></date-picker>
+  </div>
+
+</div>
+</template>
