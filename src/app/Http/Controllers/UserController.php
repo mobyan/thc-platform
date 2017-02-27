6c7f4e35afcbe9cef77bf3ed29d7300e2dd78aaa
mobyan <gomypig@gmail.com>
2017-02-27 22:58:29 +0800

    set app_id for user

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -73,7 +73,7 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        return $this->_update($id, $request->all());
     }
 
     /**
--- src/resources/assets/js/components/user.vue
+++ src/resources/assets/js/components/user.vue
@@ -3,32 +3,45 @@
         <form>
           <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" v-model="user.name" class="form-control" id="name" placeholder="name">
+            <input type="text" v-model="user.name" disabled class="form-control" id="name" placeholder="name">
         </div>
         <div class="form-group">
             <label for="type">Email</label>
-            <input type="text" v-model="user.email" class="form-control" id="type" placeholder="type">
+            <input type="text" v-model="user.email" disabled class="form-control" id="type" placeholder="type">
         </div>
         <div class="form-group">
             <label for="type">AppId</label>
-            <input type="text" v-model="user.app_id" class="form-control" id="type" placeholder="type">
-        </div>
-        <button type="submit" @click.prevent="save()" class="btn btn-default">Submit</button>
-    </form>
+            <select class="form-control" v-model="user.app_id">
+              <option v-for="app in apps" :value="app.id" >{{app.name}}</option>
+          </select>
+      </div>
+      <button type="submit" @click.prevent="save()" class="btn btn-default">Submit</button>
+  </form>
 </div>
 </template>
 <script >
-export default {
+    export default {
         data : () => {
             return {
-                user: {},
+                user: {app_id:1},
+                apps:[],
             }
         },
         created: function () {
-
-            this.$http.get('/api/user/' + this.$route.params.user).then(res=>{
-                this.user = res.body;
+            var self = this;
+            $.when(this.$http.get('/api/app/'), this.$http.get('/api/user/' + this.$route.params.user)).then(function (apps, user) {
+                self.apps = apps.body.items;
+                self.user = user.body;
             })
+        },
+        methods: {
+            save: function () {
+                this.$http.put('/api/user/' + this.$route.params.user, {
+                    app_id: this.user.app_id,
+                }).then(res => {
+
+                })
+            }
         }
     }
 </script>
