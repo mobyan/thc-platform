d68eccacbe41c81bfdf8c3234aa282b387a38a11
mobyan <gomypig@gmail.com>
2017-03-04 20:27:02 +0800

    device config

--- src/resources/assets/js/components/device.vue
+++ src/resources/assets/js/components/device.vue
@@ -1,23 +1,23 @@
 <template>
-<div>
+  <div>
     <div class="panel panel-default panel-primary">
       <div class="panel-heading" >
         <h3 class="panel-title">基本信息</h3>
       </div>
       <div class="panel-body">
-      <form>
-      <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" v-model="device.name" class="form-control" id="name" placeholder="name">
+        <form>
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" v-model="device.name" class="form-control" id="name" placeholder="name">
+          </div>
+          <div class="form-group">
+            <label for="type">type</label>
+            <input type="text" v-model="device.type" class="form-control" id="type" placeholder="type">
+          </div>
+          <button type="submit" @click.prevent="save()" class="btn btn-default">Submit</button>
+        </form>
       </div>
-      <div class="form-group">
-        <label for="type">type</label>
-        <input type="text" v-model="device.type" class="form-control" id="type" placeholder="type">
-      </div>
-      <button type="submit" @click.prevent="save()" class="btn btn-default">Submit</button>
-    </form>
-      </div>
-      </div>
+    </div>
 
     <div class="panel panel-default panel-primary">
       <div class="panel-heading" >
@@ -26,14 +26,46 @@
       <div class="panel-body">
         <form>
           <div class="form-group">
-            <label for="name">Data</label>
-            <textarea rows="3" v-model="activeConfig.data" class="form-control" id="name" placeholder="name">
-            </textarea>
+            <label for="version">Version</label>
+            <input type="text" v-model="activeConfig.id" class="form-control" id="version" placeholder="version" disabled>
+
           </div>
           <div class="form-group">
-            <label for="type">Controller</label>
-            <textarea rows="3" v-model="activeConfig.control" class="form-control" id="type" placeholder="type">
-            </textarea>
+            <label for="data">Data</label>
+            <table class="table table-bordered table-striped table-hover">
+              <tbody>
+                <tr>
+                  <th>名称</th>
+                  <th>类型</th>
+                  <th>端口</th>
+                  <th>单位</th>
+                  <th>Max</th>
+                  <th>Min</th>
+                  <th>备注</th>
+                </tr>
+                <tr v-for="(v,k) in activeConfig.data">
+                  <td><input type="text" :value="k" class="form-control" :id="k" :placeholder="k" ></td>
+                  <td><input type="text" v-model="activeConfig.data[k].type" class="form-control" :id="k" :placeholder="k" ></td>
+                  <td><input type="text" v-model="activeConfig.data[k].port" class="form-control" :id="k" :placeholder="k" ></td>
+                  <td><input type="text" v-model="activeConfig.data[k].unit" class="form-control" :id="k" :placeholder="k" ></td>
+                  <td><input type="text" v-model="activeConfig.data[k].max_v" class="form-control" :id="k" :placeholder="k" ></td>
+                  <td><input type="text" v-model="activeConfig.data[k].min_v" class="form-control" :id="k" :placeholder="k" ></td>
+                  <td><input type="text" v-model="activeConfig.data[k].desc" class="form-control" :id="k" :placeholder="k" ></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="form-group">
+            <label for="control">Controller</label>
+            <table class="table table-bordered table-striped table-hover">
+              <tbody>
+                <tr><th>名称</th><th>设置</th></tr>
+                <tr v-for="(v,k) in activeConfig.control">
+                  <td><input type="text" :value="k" class="form-control" :id="k" :placeholder="k" ></td>
+                  <td><input type="text" v-model="activeConfig.control[k]" class="form-control" :id="k" :placeholder="k" ></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
           <button type="submit" @click.prevent="addConfig()" class="btn btn-default">Submit</button>
         </form>    
@@ -79,7 +111,13 @@
         })
       },
       addConfig: function() {
-
+        var body = {
+          data: this.activeConfig.data,
+          control: this.activeConfig.control,
+        }
+        this.$http.post(this.apiURI()+'/config', body).then(function (res) {
+          this.device.configs.push(res.body)
+        })
       }
     }
   }
