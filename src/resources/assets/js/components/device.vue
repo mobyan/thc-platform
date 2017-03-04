2bacafaaf0d5ea555a520e280a896c994a861e20
mobyan <gomypig@gmail.com>
2017-03-04 22:55:04 +0800

    control config add and remove

--- src/resources/assets/js/components/device.vue
+++ src/resources/assets/js/components/device.vue
@@ -53,26 +53,30 @@
                     <td><input type="text" v-model="activeConfig.data[k].max_v" class="form-control" :id="k" :placeholder="k" ></td>
                     <td><input type="text" v-model="activeConfig.data[k].min_v" class="form-control" :id="k" :placeholder="k" ></td>
                     <td><input type="text" v-model="activeConfig.data[k].desc" class="form-control" :id="k" :placeholder="k" ></td>
-                    <td style="vertical-align: middle;"><div @click="removeData(k)"><img width="16px" height="16px" src="/image/remove.png"></div></td>
+                    <td style="vertical-align: middle;"><div @click="removeData('data', k)"><img width="16px" height="16px" src="/image/remove.png"></div></td>
 
                   </template>
                 </tr>
               </tbody>
             </table>
-            <div style="text-align: right;" ><img @click="addDataConfig()" width="16px" height="16px" src="/image/add.png"></div>
+            <div style="text-align: right;" ><img @click="addDataConfig('data')" width="16px" height="16px" src="/image/add.png"></div>
 
           </div>
           <div class="form-group">
             <label for="control">Controller</label>
             <table class="table table-bordered table-striped table-hover">
               <tbody>
-                <tr><th>名称</th><th>设置</th></tr>
+                <tr><th>名称</th><th>设置</th><th>操作</th></tr>
                 <tr v-for="(v,k) in activeConfig.control">
                   <td><input type="text" :value="k" class="form-control" :id="k" :placeholder="k" ></td>
                   <td><input type="text" v-model="activeConfig.control[k]" class="form-control" :id="k" :placeholder="k" ></td>
+                  <td style="vertical-align: middle;"><div @click="removeData('control', k)"><img width="16px" height="16px" src="/image/remove.png"></div></td>
+
                 </tr>
               </tbody>
             </table>
+            <div style="text-align: right;" ><img @click="addDataConfig('control')" width="16px" height="16px" src="/image/add.png"></div>
+
           </div>
           <button type="submit" @click.prevent="addConfig()" class="btn btn-default">Submit</button>
         </form>    
@@ -135,25 +139,24 @@
           this.device.configs.push(res.body)
         })
       },
-      removeData: function (k) {
+      removeData: function (type, k) {
         // this.device.configs[this.device.configs.length - 1].data[k] = null;
         // this.activeConfig.data[k] = null;
-        Vue.delete(this.activeConfig.data, k);
+        Vue.delete(this.activeConfig[type], k);
       },
-      addDataConfig: function () {
-        console.log(this.activeConfig)
+      addDataConfig: function (type) {
         var key = 'new';
         do {
           key = 'new_' + _.random(1,100);
-        } while(key in this.activeConfig.data)
-        Vue.set(this.activeConfig.data, key, {
+        } while(key in this.activeConfig[type])
+        Vue.set(this.activeConfig[type], key, type == 'data' ? {
           type: '',
           unit: '',
           port: '',
           max_v: '',
           min_v: '',
           desc: '',
-        })
+        } : '* * * * *')
       }
     }
   }
