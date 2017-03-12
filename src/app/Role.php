86c8370e276c538e547b4f8db2c4e0d37563b7d3
mobyan <gomypig@gmail.com>
2017-03-12 23:47:17 +0800

    permissions info

--- src/app/Role.php
+++ src/app/Role.php
@@ -5,4 +5,8 @@ use Zizaco\Entrust\EntrustRole;
 class Role extends EntrustRole
 {
     protected $fillable = ['name'];
+
+    public function permissions() {
+        return $this->belongsToMany('App\Permission');
+    }
 }
