f627861c0b8bc16dfe760c73ac8614b7e2849abe
mobyan <gomypig@gmail.com>
2017-03-13 21:02:15 +0800

    add viewer component

---
+++ src/resources/assets/js/components/viewer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div id="mask">
+            <div id="close-btn"></div>
+            <div id="mask-content">
+                <div id="mask-left"></div>
+                <div id="mask-middle">                
+                    <img :src="image.src">
+                </div>
+                <div id="mask-right"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script >
+    export default {
+        props: ['images', 'index'],
+        computed: {
+            image: function () {
+                return this.images[this.index] || {};
+            }
+        }
+    }
+</script>
