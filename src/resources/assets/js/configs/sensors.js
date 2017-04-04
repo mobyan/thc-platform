ec83f66ac3e37a16c596b650e1e65f1519e62ce1
mobyan <gomypig@gmail.com>
2017-04-04 21:13:22 +0800

    add min and max value

--- src/resources/assets/js/configs/sensors.js
+++ src/resources/assets/js/configs/sensors.js
@@ -3,59 +3,79 @@ export default [{
     "desc": "土壤传感器，采集的数据为湿度H",
     "unit": "%",
     "type": "humility",
+    "max_v": 100,
+    "min_v": 0,
     "type_desc": "湿度"
 }, {
     "name": "SOILMTYXR1_T",
     "desc": "土壤传感器，采集的数据为温度T",
     "unit": "℃",
     "type": "temperature",
+    "max_v": 60,
+    "min_v": -20,
     "type_desc": "温度"
 }, {
     "name": "CWS1806A1AG_H",
     "desc": "环境传感器, 采集的数据为湿度H",
     "unit": "%",
     "type": "humility",
+    "max_v": 100,
+    "min_v": 0,
     "type_desc": "湿度"
 }, {
     "name": "CWS1806A1AG_T",
     "desc": "环境传感器, 采集的数据为温度T",
     "unit": "℃",
     "type": "temperature",
+    "max_v": 60,
+    "min_v": -20,
     "type_desc": "温度"
 }, {
     "name": "NHZD1CI_S",
     "desc": "光照传感器, 采集的数据为光照S（lux）",
     "unit": "lux",
     "type": "solar-radiation",
+    "max_v": 200000,
+    "min_v": 0,
     "type_desc": "光照"
 }, {
     "name": "NHFS45B1_WS",
     "desc": "风速传感器，采集的数据为风速WS",
     "unit": "m/s",
     "type": "wind-velocity",
+    "max_v": 60,
+    "min_v": 0,
     "type_desc": "风速"
 }, {
     "name": "NHFX46A1_WD",
     "desc": " 风向传感器, 采集的数据为风向WD",
     "unit": "°",
     "type": "wind-direction",
+    "max_v": 359,
+    "min_v": 0,
     "type_desc": "风向"
 }, {
     "name": "KAVT2_V",
     "desc": "电压传感器，采集的数据为电压V",
     "unit": "V",
     "type": "voltage",
+    "max_v": 25,
+    "min_v": 0,
     "type_desc": "电压"
 }, {
     "name": "DAVIS785_R",
     "desc": "雨量传感器，采集的数据为降雨量R",
     "unit": "mm",
     "type": "rainfall",
+    "max_v": 0,
+    "min_v": 0,
     "type_desc": "降雨量"
 }, {
     "name": "HIKVISION_CAM_I",
     "desc": "图像传感器，采集数据为图像I",
     "unit": "",
     "type": "image",
+    "max_v": 0,
+    "min_v": 0,
     "type_desc": "图像"
 }]
