23ef8148348cdd5b43da020faf8537caf9d4c3cc
mobyan <gomypig@gmail.com>
2017-06-18 09:56:43 +0000

    add database shcema

---
+++ src/database/migrations/2017_06_18_094312_drop_app_user_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropAppUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::drop('app_user');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::create('app_user', function(Blueprint $table)
+        {
+            // $table->increments('id');
+            // $table->timestamps();
+
+            $table->integer('user_id')->unsigned();
+            $table->integer('app_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('app_id')->references('id')->on('app')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['user_id', 'app_id']);
+        });
+    }
+}
