5a34daab404dde59dae37fc904a5a6455c316b0d
Yuan Jiace <yjc_buaa@163.com>
2018-05-06 15:46:28 +0800

    为了扩展下载精度到小时 修改datepicker的配置

--- src/resources/assets/js/configs.js
+++ src/resources/assets/js/configs.js
@@ -33,7 +33,30 @@ export default {
         type: 'min',
         week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
         month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        format: 'YYYY-MM-DD HH:mm'
+        format: 'YYYY-MM-DD HH:mm',
+        placeholder: 'when?',
+        inputStyle: {
+          'display': 'inline-block',
+          'padding': '6px',
+          'line-height': '22px',
+          'font-size': '16px',
+          'border': '2px solid #fff',
+          'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.2)',
+          'border-radius': '2px',
+          // 'color': '#0d98c7'
+          'color': '#5F5F5F'
+      },
+      color: {
+          checked: '#fff',
+          header: '#ccc',
+          headerText: '#f00'
+      },
+      buttons: {
+          ok: 'Ok',
+          cancel: 'Cancel'
+      },
+      overlayOpacity: 0.5, // 0.5 as default
+      dismissible: true, // as true as default
     },
     multiOption: {
         type: 'multi-day',
